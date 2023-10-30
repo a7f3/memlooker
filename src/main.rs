215d8f8481e0b7ba1f9dbd55fe18b55423b13f78
a7f3 <124642739+a7f3@users.noreply.github.com>
2023-10-31 00:43:30 +1100

    cargo fmt'ed

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
-use std::io;
 use std::fs::DirEntry;
+use std::io;
 use std::path::Path;
 
 #[derive(Debug)]
@@ -10,7 +10,7 @@ pub struct Process {
 }
 
 impl Process {
-    pub fn name(&self) -> Result<String, io::Error>  {
+    pub fn name(&self) -> Result<String, io::Error> {
         let path_string = format!("/proc/{}/cmdline", self.pid); //fix
         let path = Path::new(&path_string);
 
@@ -25,8 +25,7 @@ impl Process {
 
     pub fn memory_regions(&self) {
         let maps_path = String::from(format!("/proc/{}/maps", self.pid));
-        let contents = fs::read_to_string(maps_path)
-            .expect("Cannot read file");
+        let contents = fs::read_to_string(maps_path).expect("Cannot read file");
         println!("{contents}");
     }
 }
@@ -34,37 +33,47 @@ impl Process {
 fn enumerate_processes() -> Option<Vec<Process>> {
     let mut procs: Vec<Process> = Vec::new();
     let path = Path::new("/proc");
-    
+
     for entry in fs::read_dir(path).expect("Unable to list") {
         let entry = entry.expect("unable to get entry");
         if entry.path().is_dir() {
-            if entry.file_name().into_string().expect("not a interger type path").parse::<u32>().is_ok() {
+            if entry
+                .file_name()
+                .into_string()
+                .expect("not a interger type path")
+                .parse::<u32>()
+                .is_ok()
+            {
                 let proc = Process {
-                    pid: entry.file_name().into_string().expect("not an interger").parse::<u32>().unwrap(), 
+                    pid: entry
+                        .file_name()
+                        .into_string()
+                        .expect("not an interger")
+                        .parse::<u32>()
+                        .unwrap(),
                     dir: entry,
                 };
                 procs.push(proc);
             }
         }
     }
-    
+
     if procs.len() > 0 {
-        return Some(procs)
+        return Some(procs);
     }
-    return None
+    return None;
 }
 fn print_all_procs() {
     let procs = enumerate_processes().unwrap_or(Vec::new());
 
-    procs
-        .iter()
-        .for_each(|proc| println!( "{}: {}", 
+    procs.iter().for_each(|proc| {
+        println!(
+            "{}: {}",
             proc.pid,
             proc.name().unwrap_or(String::from("<<NONE>>"))
-        ));
-
+        )
+    });
 }
-
 
 fn main() {
     let procs = enumerate_processes().unwrap_or(Vec::new());
@@ -75,5 +84,4 @@ fn main() {
             dbg!(proc.name().unwrap());
         }
     }
-
 }
